b228d92a7921e6df52dbad67a9bdb93760a0721a
SimonLiu423 <simonliu423@gmail.com>
2024-03-09 01:56:59 +0800

    assignment 3 part 1

--- examples/systemcalls/systemcalls.c
+++ examples/systemcalls/systemcalls.c
@@ -1,4 +1,8 @@
 #include "systemcalls.h"
+#include "stdlib.h"
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/wait.h>
 
 /**
  * @param cmd the command to execute with system()
@@ -16,7 +20,9 @@ bool do_system(const char *cmd)
  *   and return a boolean true if the system() call completed with success
  *   or false() if it returned a failure
 */
-
+    
+    int ret = system(cmd);
+    if (ret == -1) return false;
     return true;
 }
 
@@ -58,9 +64,21 @@ bool do_exec(int count, ...)
  *   as second argument to the execv() command.
  *
 */
-
+    pid_t pid =  fork();
+    printf("pid=%d\n", pid);
+    if (pid < 0){
+        return false;
+    } else if (pid > 0) {
+        int status;
+        pid_t res = waitpid(pid, &status, 0);
+        if (res < 0 || !(WIFEXITED(status) == true && WEXITSTATUS(status) == EXIT_SUCCESS)) {
+            return false;
+        }
+    } else {
+        execv(command[0], command);
+        _exit(EXIT_FAILURE);
+    }
     va_end(args);
-
     return true;
 }
 
@@ -92,6 +110,25 @@ bool do_exec_redirect(const char *outputfile, int count, ...)
  *   The rest of the behaviour is same as do_exec()
  *
 */
+    int fd = open(outputfile, O_WRONLY|O_TRUNC|O_CREAT, 0644);
+    if (fd < 0) { perror("open"); abort(); }
+
+    int kidpid = fork();
+    if(kidpid == -1){ perror("fork"); abort();}
+    else if (kidpid > 0) {
+        close(fd);
+        /* do whatever the parent wants to do. */
+        int status;
+        pid_t res = waitpid(kidpid, &status, 0);
+        if (res < 0 || !(WIFEXITED(status) == true && WEXITSTATUS(status) == EXIT_SUCCESS)) {
+            return false;
+        }
+    }
+    else{
+        if (dup2(fd, 1) < 0) { perror("dup2"); abort(); }
+        close(fd);
+        execv(command[0], command); perror("execv"); abort();
+    }
 
     va_end(args);
 
